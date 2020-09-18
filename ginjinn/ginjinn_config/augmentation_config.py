7c06e2392da0915b6974c5e90c2148ab9b56c26f
TankredO <tankred.ott@ur.de>
2020-09-18 09:29:02 +0200

    refactored configs into submodules

---
+++ ginjinn/ginjinn_config/augmentation_config.py
@@ -0,0 +1,29 @@
+'''
+GinJinn augmentation configuration module
+'''
+
+import copy
+from typing import Optional
+
+class GinjinnAugmentationConfiguration: #pylint: disable=too-few-public-methods
+    '''A class representin GinJinn augmentation configurations.
+    '''
+
+    @classmethod
+    def from_dictionary(cls, config: dict):
+        '''Build GinjinnAugmentationConfiguration from a dictionary object.
+
+        Parameters
+        ----------
+        config : dict
+            Dictionary object containing the augmentation configuration.
+
+        Returns
+        -------
+        GinjinnAugmentationConfiguration
+            GinjinnAugmentationConfiguration constructed with the configuration
+            given in config.
+        '''
+
+        # TODO: implement
+        return cls()
