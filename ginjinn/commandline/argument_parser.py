319e2e6bd417b91d0ae2f2ec98997387b054a9af
TankredO <tankred.ott@ur.de>
2020-10-23 09:35:48 +0200

    subparsers init

--- ginjinn/commandline/argument_parser.py
+++ ginjinn/commandline/argument_parser.py
@@ -4,13 +4,28 @@
 import argparse
 
 # TODO: implement
+
+# Note: It is a deliberate decision not to subclass argparse.ArgumentParser.
+#       It might be preferable to work with composition instead of inheritance,
+#       since it might be desirable to include postprocessing steps after argparse
+#       parsing.
 class GinjinnArgumentParser():
     '''GinjinnArgumentParser
 
     Class for setting up and handling commandline arguments.
     '''
+
+    _description = '''
+        GinJinn is a framework for simplifying the setup, training, evaluation,
+        and deployment of object detection models.
+    '''
+
     def __init__(self):
-        self.parser = argparse.ArgumentParser()
+        self.parser = argparse.ArgumentParser(
+            description=self._description
+        )
+
+        self._subparsers = self.parser.add_subparsers()
 
     def parse_args(self):
         '''parse_args
@@ -23,3 +38,10 @@ class GinjinnArgumentParser():
             Parsed argparse arguments
         '''
         return self.parser.parse_args()
+
+    def _init_subparsers(self):
+        '''_init_subparsers
+
+        Initilialize parsers for GinJinn subcommands.
+        '''
+        # TODO: implement
