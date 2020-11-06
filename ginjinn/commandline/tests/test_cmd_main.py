417975c3b7713d4db32cd0408f804abb06b0b9c1
TankredO <tankred.ott@ur.de>
2020-11-06 08:54:02 +0100

    commandline tests

--- ginjinn/commandline/tests/test_cmd_main.py
+++ ginjinn/commandline/tests/test_cmd_main.py
@@ -4,10 +4,13 @@ import copy
 import tempfile
 import os
 import mock
+import pkg_resources
+import yaml
 
 from ginjinn.commandline import main, commandline_app, argument_parser
 from ginjinn.commandline import splitter
 from ginjinn.commandline import simulate
+from ginjinn.commandline import train
 
 from ginjinn.simulation import generate_simple_shapes_coco
 
@@ -32,6 +35,41 @@ def simulate_coco(tmp_dir):
     )
     return img_dir, ann_path
 
+@pytest.fixture(scope='module', autouse=True)
+def example_config(tmp_dir, simulate_coco):
+    img_dir, ann_path = simulate_coco
+
+    example_config_1_path = pkg_resources.resource_filename(
+        'ginjinn', 'data/ginjinn_config/example_config_1.yaml',
+    )
+
+    with open(example_config_1_path) as config_f:
+        config = yaml.load(config_f)
+
+    config['input']['train']['annotation_path'] = ann_path
+    config['input']['train']['image_path'] = img_dir
+
+    config_dir = os.path.join(tmp_dir, 'example_config')
+    os.mkdir(config_dir)
+
+    config_file = os.path.join(config_dir, 'config_0.yaml')
+    with open(config_file, 'w') as config_f:
+        yaml.dump(config, config_f)
+
+    return (config, config_file)
+
+@pytest.fixture(scope='module', autouse=True)
+def example_project(tmp_dir, example_config):
+    config, _ = example_config
+
+    project_dir = os.path.join(tmp_dir, 'example_project')
+    os.mkdir(project_dir)
+
+    config_file = os.path.join(project_dir, 'ginjinn_config.yaml')
+    with open(config_file, 'w') as config_f:
+        yaml.dump(config, config_f)
+    
+    return project_dir
 
 def test_main_simple(tmp_dir):
     project_dir = os.path.join(tmp_dir, 'test_new_0')
@@ -79,7 +117,6 @@ def test_splitting(tmp_dir, simulate_coco):
 
 def test_simulate(tmp_dir):
     simulate_dir = os.path.join(tmp_dir, 'test_simulate_0')
-    os.mkdir(simulate_dir)
 
     args = argument_parser.GinjinnArgumentParser().parse_args(
         [
@@ -90,8 +127,18 @@ def test_simulate(tmp_dir):
         ]
     )
 
-    with mock.patch('builtins.input', lambda *args: 'y'):
-        simulate.ginjinn_simulate(args)
+    simulate.ginjinn_simulate(args)
 
     with mock.patch('builtins.input', lambda *args: 'y'):
         simulate.ginjinn_simulate(args)
+
+def test_train(example_project):
+    project_dir = example_project
+    args = argument_parser.GinjinnArgumentParser().parse_args(
+        [
+            'train',
+            project_dir
+        ]
+    )
+
+    train.ginjinn_train(args)
