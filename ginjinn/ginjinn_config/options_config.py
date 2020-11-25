42eda5c2e7f5f4ba22a1825b16d6a10c17c776fd
TankredO <tankred.ott@ur.de>
2020-11-25 08:44:29 +0100

    options config

--- ginjinn/ginjinn_config/options_config.py
+++ ginjinn/ginjinn_config/options_config.py
@@ -23,9 +23,11 @@ class GinjinnOptionsConfiguration: #pylint: disable=too-few-public-methods
         self,
         resume: bool,
         n_threads: int,
+        device: str,
     ):
         self.resume = resume
         self.n_threads = n_threads
+        self.device = device
 
         self._check_config()
 
@@ -41,6 +43,7 @@ class GinjinnOptionsConfiguration: #pylint: disable=too-few-public-methods
         '''
 
         cfg.DATALOADER.NUM_WORKERS = self.n_threads
+        cfg.MODEL.DEVICE = self.device
 
     @classmethod
     def from_dictionary(cls, config: dict):
@@ -61,6 +64,7 @@ class GinjinnOptionsConfiguration: #pylint: disable=too-few-public-methods
         default_config = {
             'resume': False,
             'n_threads': N_CORES - 1 if N_CORES > 1 else N_CORES,
+            'device': 'cuda',
         }
 
         # Maybe implement this more elegantly...
@@ -70,6 +74,7 @@ class GinjinnOptionsConfiguration: #pylint: disable=too-few-public-methods
         return cls(
             resume=config['resume'],
             n_threads=config['n_threads'],
+            device=config['device'],
         )
 
     def _check_n_threads(self):
@@ -85,7 +90,31 @@ class GinjinnOptionsConfiguration: #pylint: disable=too-few-public-methods
                 'n_threads must be a positive number.'
             )
 
+    def _check_device(self):
+        '''_check_device
+
+        Raises
+        ------
+        InvalidOptionsConfigurationError
+            Raised if device value is invalid.
+        '''
+        err_msg = f'Invalid device option "{self.device}"; device should be either "cpu", ' +\
+            '"cuda", or "cpu:" or "cuda:" followed by the respective device number (e.g "cuda:0").'
+
+        if self.device in ['cpu', 'cuda']:
+            return
+
+        if self.device.startswith('cpu:') or self.device.startswith('cuda:'):
+            try:
+                int(self.device.split(':')[1])
+            except Exception as err:
+                raise InvalidOptionsConfigurationError(err_msg) from err
+        else:
+            raise InvalidOptionsConfigurationError(err_msg)
+
+
     def _check_config(self):
         ''' Check configuration values for validity.
         '''
         self._check_n_threads()
+        self._check_device()
--- ginjinn/ginjinn_config/tests/test_options_config.py
+++ ginjinn/ginjinn_config/tests/test_options_config.py
@@ -8,12 +8,16 @@ from ginjinn.ginjinn_config.config_error import InvalidOptionsConfigurationError
 def test_simple():
     n_threads=1
     resume=True
+    device='cuda'
+
     options_0 = GinjinnOptionsConfiguration(
         resume=resume,
-        n_threads=n_threads
+        n_threads=n_threads,
+        device=device,
     )
     assert options_0.resume == resume
     assert options_0.n_threads == n_threads
+    assert options_0.device == device
 
     options_1 = GinjinnOptionsConfiguration.from_dictionary({
         'n_threads': n_threads,
@@ -21,13 +25,51 @@ def test_simple():
     })
     assert options_1.resume == resume
     assert options_1.n_threads == n_threads
+    assert options_1.device == device
+
+    options_2 = GinjinnOptionsConfiguration(
+        resume=resume,
+        n_threads=n_threads,
+        device='cpu',
+    )
+    assert options_2.resume == resume
+    assert options_2.n_threads == n_threads
+    assert options_2.device == 'cpu'
+
+    GinjinnOptionsConfiguration(
+        resume=resume,
+        n_threads=n_threads,
+        device='cpu:0',
+    )
+
+    GinjinnOptionsConfiguration(
+        resume=resume,
+        n_threads=n_threads,
+        device='cuda:0',
+    )
 
 def test_invalid():
     n_threads=0
     resume=True
+    device='cuda'
 
     with pytest.raises(InvalidOptionsConfigurationError):
         GinjinnOptionsConfiguration(
             resume=resume,
-            n_threads=n_threads
+            n_threads=n_threads,
+            device=device,
+        )
+
+    with pytest.raises(InvalidOptionsConfigurationError):
+        GinjinnOptionsConfiguration(
+            resume=resume,
+            n_threads=2,
+            device='',
+        )
+    
+    with pytest.raises(InvalidOptionsConfigurationError):
+        GinjinnOptionsConfiguration(
+            resume=resume,
+            n_threads=2,
+            device='cuda:x',
         )
