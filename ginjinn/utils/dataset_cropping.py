7f9a88cd344fee8b0efb2ad37c03854c6dc5cdd2
ulilautenschlager <ulrich.lautenschlager@ur.de>
2021-02-03 08:50:35 +0100

    crop_seg_annotations() added

--- ginjinn/utils/dataset_cropping.py
+++ ginjinn/utils/dataset_cropping.py
@@ -6,10 +6,113 @@ import datetime
 import glob
 import json
 import os
+from typing import List, Sequence, Tuple
 import numpy as np
 import cv2
 import imantics
 from pycocotools import mask
+
+
+# pylint: disable=C0103
+def crop_seg_annotations(
+    annotations: List,
+    img_width: int,
+    img_height: int,
+    cropping_range: Sequence[int],
+    start_id: int
+) -> Tuple:
+    """Crop annotated segmentation instances.
+
+    This function transforms a list of object annotations in COCO format, such that the resulting
+    annotations refer to a cropped version of the original image. The output only contains objects
+    with valid, non-empty segmentation.
+
+    Parameters
+    ----------
+    annotations : list of dict
+        List of object annotations referring to the same image.
+    img_width : int
+        (Original) image width
+    img_height : int
+        (Original) image height
+    cropping_range: sequence of int
+        (x0, x1, y0, y1) slices segmentation masks by x0:x1 (horizontally) and y0:y1 (vertically).
+    start_id : int
+        Object ID to start output annotations with.
+
+    Returns
+    -------
+    next_id
+        This may be useful as start id for further objects.
+    annotation_cropped
+        List of transformed COCO annotations with non-empty segmentation
+
+    Raises
+    ------
+    TypeError
+        Raised for unsupported segmentation format.
+    ValueError
+        Raised if input annotations refer to different images.
+    """
+    x_start, x_end, y_start, y_end = cropping_range
+    annotations_cropped = []
+    img_id = None
+    i_ann = start_id
+
+    for annotation in annotations:
+        # check image id
+        if img_id:
+            if annotation["image_id"] != img_id:
+                raise ValueError(
+                    "All annotations must refer to the same image, i.e., have equal image_id's."
+                )
+        else:
+            img_id = annotation["image_id"]
+
+        # read segmentation
+        seg_orig = annotation.get("segmentation")
+        if seg_orig:
+            if isinstance(seg_orig, dict):
+                # rle to mask
+                imask = imantics.Mask(mask.decode(seg_orig).astype("bool"))
+            elif isinstance(seg_orig, list):
+                # polygon to mask
+                ipolygons = imantics.Polygons(seg_orig)
+                imask = ipolygons.mask(img_width, img_height)
+            else:
+                raise TypeError(
+                    "Unknown segmentation format, polygons or RLE expected"
+                )
+        else:
+            # skip instances without segmentation
+            continue
+
+        # crop segmentation
+        imask_cropped = imantics.Mask(imask.array[y_start:y_end, x_start:x_end])
+        seg_cropped = imask_cropped.polygons().segmentation
+
+        if seg_cropped:
+            # calculate bounding box
+            x_any = imask_cropped.array.any(axis=0)
+            y_any = imask_cropped.array.any(axis=1)
+            x = np.where(x_any)[0].tolist()
+            y = np.where(y_any)[0].tolist()
+            x1, y1, x2, y2 = (x[0], y[0], x[-1] + 1, y[-1] + 1)
+            bbox_coco = [ x1, y1, x2 - x1, y2 - y1 ]
+
+            # create object annotation
+            annotations_cropped.append({
+                "area": (x2 - x1) * (y2 - y1),
+                "bbox": bbox_coco,
+                "segmentation": seg_cropped,
+                "iscrowd": 0,
+                "image_id": img_id,
+                "id": i_ann,
+                "category_id": annotation["category_id"]
+            })
+            i_ann += 1
+
+    return i_ann, annotations_cropped
 
 
 # pylint: disable=C0103
