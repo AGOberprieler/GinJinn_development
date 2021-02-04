ca02a7bd6e5f6359aa8dc03382c8b8622586d7a4
ulilautenschlager <ulrich.lautenschlager@ur.de>
2021-02-04 14:47:08 +0100

    filter categories (COCO)

--- ginjinn/utils/data_prep.py
+++ ginjinn/utils/data_prep.py
@@ -4,6 +4,8 @@
 import json
 import os
 import shutil
+from typing import List
+import datetime
 import pandas as pd
 
 def flatten_coco(
@@ -83,3 +85,70 @@ def flatten_coco(
             columns=['id', 'original_path']
         )
         id_map_df.to_csv(id_map_file, index=False)
+
+
+def filter_categories_coco(
+    ann_file: str,
+    out_file: str,
+    drop: List[str] = None,
+    keep: List[str] = None
+):
+    """filter_categories_coco
+
+    This function allows to filter object annotations in a COCO dataset according to their
+    assigned category. Therefore, either ``drop`` or ``keep`` has to be specified.
+    Note that the original IDs referring to annotations, categories and images are preserved,
+    and may be non-contiguous in the output dataset.
+
+    Parameters
+    ----------
+    ann_file : str
+        Path to annotation (JSON) file
+    out_file : str
+        Output file name
+    drop : list of str
+        If specified, these categories are removed from the dataset.
+    keep : list of str
+        If specified, only these categories are preserved.
+
+    Raises
+    ------
+    ValueError
+        Raised for unsupported parameter settings.
+    """
+    if bool(drop) + bool(keep) == 0:
+        raise ValueError(
+            "Either ``drop`` or ``keep`` has to be specified as non-empty list."
+        )
+
+    with open(ann_file, "rb") as f:
+        anno = json.load(f)
+
+    if keep:
+        categories_left = {cat["id"] for cat in anno.get("categories") if cat["name"] in keep}
+    else:
+        categories_left = {cat["id"] for cat in anno.get("categories") if cat["name"] not in drop}
+
+    anno["annotations"] = [ann for ann in anno.get("annotations") if ann["category_id"] in categories_left]
+    anno["categories"] = [cat for cat in anno.get("categories") if cat["id"] in categories_left]
+
+    images_left = {ann["image_id"] for ann in anno.get("annotations")}
+    anno["images"] = [img for img in anno.get("images") if img["id"] in images_left]
+
+    anno["info"] = {
+        "contributor" : "",
+        "date_created" : datetime.datetime.now().strftime("%Y/%m/%d"),
+        "description" : "",
+        "version" : "",
+        "url" : "",
+        "year" : ""
+    }
+
+    # write COCO json file
+    with open(out_file, 'w') as json_file:
+        json.dump(
+            anno,
+            json_file,
+            indent = 2,
+            sort_keys = True
+        )
